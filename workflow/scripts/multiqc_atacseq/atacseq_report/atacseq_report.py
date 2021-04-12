89a03f6eb605ee9c860014c46962ee37b3c5ddd8
sreichl <53785552+sreichl@users.noreply.github.com>
2021-04-12 17:20:13 +0200

    fix symlink issue

--- workflow/scripts/multiqc_atacseq/atacseq_report/atacseq_report.py
+++ workflow/scripts/multiqc_atacseq/atacseq_report/atacseq_report.py
@@ -44,7 +44,7 @@ def atacseq_report_execution_start():
     if hasattr(config, 'base_url') and hasattr(config, 'project_uuid') and hasattr(config, 'public_html_folder'):
         project_url = os.path.join(config.base_url, config.project_uuid)
         os.chdir(config.public_html_folder)
-        if not os.path.exists(os.path.join(config.public_html_folder, config.project_uuid)):
+        if not os.path.islink(os.path.join(config.public_html_folder, config.project_uuid)):
             # The symlink has to be relative so that the web server can locate the project folder
             relative_path = os.path.relpath(config.project_path)
             os.symlink(relative_path, config.project_uuid)
